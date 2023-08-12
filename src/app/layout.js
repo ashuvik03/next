085cb4b1ba14ca1897190eebcd7681551f7557d1
vikramsengar <vikram.sengar@affinityeducation.in>
2023-08-12 12:20:51 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from './Footer/page'
+import Header from './Header/page'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   )
 }
